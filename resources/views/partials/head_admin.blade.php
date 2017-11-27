62e747cb745c112ef7af02e53987026b66eb6fce
Gilang Darmawan <jobs.gilangdarmawan@gmail.com>
2017-11-27 14:13:18 +0700

    Edit uploaded image di create banner

--- resources/views/partials/head_admin.blade.php
+++ resources/views/partials/head_admin.blade.php
@@ -13,5 +13,10 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.4.5/css/fileinput.min.css" media="all" rel="stylesheet" type="text/css" />
 <!-- Page level plugin CSS-->
 <link href="https://blackrockdigital.github.io/startbootstrap-sb-admin/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+<!-- bootstrap validator -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.2/css/bootstrapValidator.min.css" rel="stylesheet">
 <!-- Custom styles for this template-->
 <link href="https://blackrockdigital.github.io/startbootstrap-sb-admin/css/sb-admin.css" rel="stylesheet">
+<style type="text/css">
+.table img {max-width: 100px;height: auto;width: 100%;}
+</style>
