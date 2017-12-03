3645d283ad72d09c6f02f071ed60c4fa74e1b2ad
muhammadlintang <muhammadlintang.arip@gmail.com>
2017-12-03 19:30:07 +0700

    test

--- resources/views/dashboard/make_sushi/index.blade.php
+++ resources/views/dashboard/make_sushi/index.blade.php
@@ -1,39 +1,33 @@
 @extends('layouts.dashboard')
 
-<link rel="stylesheet" type="text/css" href="{{asset('vendors/datatables/css/dataTables.bootstrap.css')}}" />
-<link rel="stylesheet" type="text/css" href="{{asset('vendors/datatables/css/buttons.bootstrap.css')}}" />
-<link rel="stylesheet" type="text/css" href="{{asset('vendors/datatables/css/colReorder.bootstrap.css')}}" />
-<link rel="stylesheet" type="text/css" href="{{asset('vendors/datatables/css/dataTables.bootstrap.css')}}" />
-<link rel="stylesheet" type="text/css" href="{{asset('vendors/datatables/css/rowReorder.bootstrap.css')}}" />
-<link rel="stylesheet" type="text/css" href="{{asset('vendors/datatables/css/buttons.bootstrap.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('vendors/datatables/css/scroller.bootstrap.css')}}">
-<link href="{{asset('vendors/hover/css/hover-min.css')}}" rel="stylesheet">
-<link href="{{asset('css/buttons_sass.css')}}" rel="stylesheet">
-
 @section('content')
 <!-- Breadcrumbs-->
 <ol class="breadcrumb">
     <li class="breadcrumb-item">
         <a href="/dashboard">Dashboard</a>
     </li>
-    <li class="breadcrumb-item active">Banner</li>
+    <li class="breadcrumb-item active">Ingredients</li>
 </ol>
 <!-- table -->
 <div class="card mb-3">
     <div class="card-header">
         <div class="d-flex align-items-center">
-            Banner List
-            <a href="/dashboard/banner/create" class="btn btn-success ml-auto"><i class="fa fa-plus-circle fa-fw"></i>Add Banner</a>
+            Ingredients List
+            <a href="/dashboard/make_sushi/create" class="btn btn-success ml-auto"><i class="fa fa-plus-circle fa-fw"></i>Add Ingredients</a>
         </div>
     </div>
     <div class="card-body">
         <div class="table-responsive">
-            <table class="table table-bordered" width="1500px" cellspacing="0" id="tables">
+            <table class="table table-bordered" cellspacing="0" id="tables" style="width: 100%;min-width: 1300px;">
                 <thead>
                     <tr>
                         <th>ID</th>
                         <th>Images</th>
-                        <th>Title</th>
+                        <th>Nama File</th>
+                        <th>Name</th>
+                        <th>Categories</th>
+                        <th>Price</th>
+                        <th>Stock</th>
                         <th>Description</th>
                         <th>Status</th>
                         <th>Action</th>
@@ -46,53 +40,41 @@
 </div>
 <script src="{{asset('js/jquery-1.11.1.min.js')}}" type="text/javascript"></script>
 <script>
-    $(document).ready(function () {
-        var table = $('#tables').DataTable({
-            // dom: 'Bflrtip',
+$(document).ready(function () {
+    var table = $('#tables').DataTable({
+        // dom: 'Bflrtip',
 
-            "processing": true,
-            "serverSide": true,
-            "ajax":{
-                     "url": "/dashboard/banner",
-                     "dataType": "json",
-                     "type": "POST"
-                   },
-            success: function(data) {
-                 alert('success')
-              },
-              error: function (xhr, ajaxOptions, thrownError) {
-                alert(xhr.status);
-                alert(thrownError);
-              },
-            "columns": [
-                { "data": "id" , name:"id"},
-                { "data": "url_img_banner" , name:"url_img_banner"},
-                { "data": "title" , name:"title"},
-                { "data": "description" , name:"nama_kel"},
-                { "data": "status" , name:"status"},
-                { "data": "option" , name:"option",orderable:false}
-            ],
-            "order": [[ 1, "desc" ]]
-        });
-        $('#tables_filter input').unbind();
-        $('#tables_filter input').bind('keyup', function(e) {
+        "processing": true,
+        "serverSide": true,
+        "ajax":{
+                 "url": "/dashboard/make_sushi",
+                 "dataType": "json",
+                 "type": "POST"
+               },
+
+        "columns": [
+            { "data": "id" , "name":"id"},
+            { "data": "images", "render": function(data, type, row) {
+                return '<img src="'+data+'" />';
+                }
+            },
+            { "data": "url_img_ingredients" , name:"url_img_ingredients"},
+            { "data": "name" , name:"name"},
+            { "data": "categories" , name:"categories"},
+            { "data": "price" , name:"price"},
+            { "data": "stock" , name:"stock"},
+            { "data": "description" , name:"description"},
+            { "data": "status" , name:"status"},
+            { "data": "option" , name:"option",orderable:false}
+        ],
+        "order": [[ 0, "desc" ]]
+    });
+    $('#tables_filter input').unbind();
+    $('#tables_filter input').bind('keyup', function(e) {
         if(e.keyCode == 13) {
             table.search(this.value).draw();
         }
-    })
+    });
 });
 </script>
-<script type="text/javascript" src="{{asset('vendors/datatables/js/jquery.dataTables.js')}}"></script>
-<script type="text/javascript" src="{{asset('vendors/datatables/js/buttons.html5.js')}}"></script>
-<script type="text/javascript" src="{{asset('vendors/datatables/js/dataTables.bootstrap.js')}}"></script>
-<script type="text/javascript" src="{{asset('vendors/datatables/js/dataTables.buttons.js')}}"></script>
-<script type="text/javascript" src="{{asset('vendors/datatables/js/dataTables.colReorder.js')}}"></script>
-<script type="text/javascript" src="{{asset('vendors/datatables/js/dataTables.responsive.js')}}"></script>
-<script type="text/javascript" src="{{asset('vendors/datatables/js/dataTables.rowReorder.js')}}"></script>
-<script type="text/javascript" src="{{asset('vendors/datatables/js/buttons.colVis.js')}}"></script>
-<script type="text/javascript" src="{{asset('vendors/datatables/js/buttons.html5.js')}}"></script>
-<script type="text/javascript" src="{{asset('vendors/datatables/js/buttons.bootstrap.js')}}"></script>
-<script type="text/javascript" src="{{asset('vendors/datatables/js/buttons.print.js')}}"></script>
-<script type="text/javascript" src="{{asset('vendors/datatables/js/dataTables.scroller.js')}}"></script>
-<script src="{{asset('js/custom_js/alert.js')}}" type="text/javascript"></script>
 @stop
