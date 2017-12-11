3be98ef7460a3acf459715b74cb23a3897de04e0
muhammadlintang <muhammadlintang.arip@gmail.com>
2017-12-11 16:33:45 +0700

    lintang-ambil

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,10 +1,9 @@
 <footer class="footer">
     <ul class="list-inline">
         <li class="list-inline-item"><a href="#">TERM & CONDITION</a></li>
-        <li class="list-inline-item"><a href="#">CONTACT US</a></li>
         <li class="list-inline-item"><a href="#">REFUND POLICY</a></li>
-        <li class="list-inline-item"><a href="#">PAYMENT METHOD</a></li>
-        <li class="list-inline-item"><a href="#">SHIPPING METHOD</a></li>
+        <li class="list-inline-item"><a href="#">FAQ</a></li>
+        <li class="list-inline-item"><a href="#">CONTACT US</a></li>
     </ul>
     &copy; COPYRIGHT BAIZA SUSHI 2017. ALL RIGHT RESERVED
 </footer>
